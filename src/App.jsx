6048e4392f6f3f77986b426ae34f31f923064eaf
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2023-09-27 16:29:59 +0200

    App.jsx > Add my FirstName

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  // state (état, données)
-  const [prenom, setPrenom] = useState("Mathis");
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
 
-  // comportements
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
-  // affichage (render)
   return (
-    <div>
-      <h1>Bonjour {prenom}</h1>
-      <p>{count}</p>
-      <button onClick={handleClick}>Incrémenter</button>
-    </div>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>Salut Mathis</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   );
 }
 
