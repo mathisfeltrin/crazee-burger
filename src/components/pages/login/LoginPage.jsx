843cc474eb97cc14adcb7569d21be2e09b4b5b65
mathisfeltrin <mathisfeltrin@gmail.com>
2023-11-01 19:47:57 +0100

    💄 F03: LoginPage > add layout

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import Logo from "../../reusable-ui/Logo";
 import LoginForm from "./LoginForm";
+import styled from "styled-components";
 
 export default function LoginPage() {
   // state
@@ -7,5 +8,19 @@ export default function LoginPage() {
   // comportements
 
   // affichage (render)
-  return <LoginForm />;
+  return (
+    <LoginPageStyled>
+      <Logo />
+      <LoginForm />
+    </LoginPageStyled>
+  );
 }
+
+const LoginPageStyled = styled.div`
+  background: red;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
