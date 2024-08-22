fcf82c50714720fbe9083ad2883f39949e8e0be3
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2024-08-22 15:45:05 +0200

    ✨  F02: LOGIN > add redirection to ErrorPage on Submitwith username

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function LoginForm() {
   // state
   const [inputValue, setInputValue] = useState("");
+  const navigate = useNavigate();
 
   // comportement
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(`Bonjour ${inputValue}`);
     setInputValue("");
+    navigate(`order/${inputValue}`);
   };
 
   const handleChange = (event) => {
@@ -31,7 +32,6 @@ export default function LoginForm() {
         required
       />
       <button>Accédez à votre espace</button>
-      <Link to="/order">Vers OrderPage</Link>
     </form>
   );
 }
