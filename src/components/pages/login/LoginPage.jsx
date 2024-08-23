172b3e232e317756b58497d8a3d4a7575c0d31c6
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2024-08-23 14:16:51 +0200

    💄  F03: LOGIN > Add styling and background

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
 import LoginForm from "./LoginForm";
 import styled from "styled-components";
 import Logo from "../../reusable-ui/Logo";
 
 export default function LoginPage() {
-  // state
-
-  // comportements
-
-  // affichage (render)
   return (
     <LoginPageStyled>
       <Logo />
@@ -18,10 +12,24 @@ export default function LoginPage() {
 }
 
 const LoginPageStyled = styled.div`
-  background: red;
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  &::before {
+    content: "";
+    background: url("/images/burger-background.jpg") rgba(0, 0, 0, 0.7);
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
 `;
