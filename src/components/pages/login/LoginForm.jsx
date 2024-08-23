e6e369f8d1ed9437eb92582c6d866d669aaa3eb6
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2024-08-23 15:13:21 +0200

    💄  F03: LOGIN > LoginForm > input > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { BsPersonCircle } from "react-icons/bs";
 
 export default function LoginForm() {
   // state
@@ -22,17 +23,24 @@ export default function LoginForm() {
   // affichage (render)
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous !</h1>
-      <hr />
-      <h2>Connectez-vous</h2>
-      <input
-        value={inputValue}
-        onChange={handleChange}
-        type="text"
-        placeholder="Entrez votre prénom"
-        required
-      />
-      <button>Accéder à mon espace</button>
+      <div>
+        <h1>Bienvenue chez nous !</h1>
+        <hr />
+        <h2>Connectez-vous</h2>
+      </div>
+      <div>
+        <div className="input-with-icon">
+          <BsPersonCircle className="icon" />
+          <input
+            value={inputValue}
+            onChange={handleChange}
+            type="text"
+            placeholder="Entrez votre prénom"
+            required
+          />
+        </div>
+        <button>Accéder à mon espace</button>
+      </div>
     </LoginFormStyled>
   );
 }
@@ -62,5 +70,36 @@ const LoginFormStyled = styled.form`
     margin: 20px 10px 10px;
     color: white;
     font-size: 36px;
+  }
+
+  .input-with-icon {
+    /* border: 1px solid red; */
+    background-color: #fff;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding: 18px 24px;
+    max-width: 18px 0; // could be handle in Parent too
+    /* white-space: nowrap; */
+
+    .icon {
+      font-size: 15px;
+      margin-right: 8px;
+      color: #93a2b1;
+      /* min-width: 1em; // that way, the icon size is NOT affected */
+    }
+
+    input {
+      border: none;
+      font-size: 15px;
+      color: #17161a;
+      /* width: 100%; */
+      /* display: flex; */
+    }
+
+    &::placeholder {
+      background: white;
+      color: lightgray;
+    }
   }
 `;
