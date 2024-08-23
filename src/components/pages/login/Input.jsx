936c23d72e2eab25fdcb45c93d72dd87b7a3db44
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2024-08-23 19:22:47 +0200

    ♻️  F03: LOGIN > LoginForm > Create reusable <Input/>

---
+++ src/components/pages/login/Input.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export default function Input({ value, onChange, Icon, ...otherProps }) {
+  console.log(otherProps);
+  return (
+    <InputStyled>
+      {Icon && Icon}
+      <input value={value} onChange={onChange} type="text" {...otherProps} />
+    </InputStyled>
+  );
+}
+
+const InputStyled = styled.div`
+  /* border: 1px solid red; */
+  background-color: #fff;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  padding: 18px 24px;
+  max-width: 18px 0; // could be handle in Parent too
+  /* white-space: nowrap; */
+
+  .icon {
+    font-size: 15px;
+    margin-right: 8px;
+    color: #93a2b1;
+    /* min-width: 1em; // that way, the icon size is NOT affected */
+  }
+
+  input {
+    border: none;
+    font-size: 15px;
+    color: #17161a;
+    /* width: 100%; */
+    /* display: flex; */
+  }
+
+  &::placeholder {
+    background: white;
+    color: lightgray;
+  }
+`;
--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { BsPersonCircle } from "react-icons/bs";
 import { IoChevronForward } from "react-icons/io5";
+import Input from "./Input";
 
 export default function LoginForm() {
   // state
@@ -30,16 +31,13 @@ export default function LoginForm() {
         <h2>Connectez-vous</h2>
       </div>
       <div>
-        <div className="input-with-icon">
-          <BsPersonCircle className="icon" />
-          <input
-            value={inputValue}
-            onChange={handleChange}
-            type="text"
-            placeholder="Entrez votre prénom"
-            required
-          />
-        </div>
+        <Input
+          value={inputValue}
+          onChange={handleChange}
+          placeholder={"Entrez votre prénom"}
+          required
+          Icon={<BsPersonCircle className="icon" />}
+        />
         <button className="login-button">
           <span>Accéder à mon espace</span>
           <IoChevronForward className="icon" />
@@ -73,37 +71,6 @@ const LoginFormStyled = styled.form`
     margin: 20px 10px 10px;
     color: white;
     font-size: 36px;
-  }
-
-  .input-with-icon {
-    /* border: 1px solid red; */
-    background-color: #fff;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    padding: 18px 24px;
-    max-width: 18px 0; // could be handle in Parent too
-    /* white-space: nowrap; */
-
-    .icon {
-      font-size: 15px;
-      margin-right: 8px;
-      color: #93a2b1;
-      /* min-width: 1em; // that way, the icon size is NOT affected */
-    }
-
-    input {
-      border: none;
-      font-size: 15px;
-      color: #17161a;
-      /* width: 100%; */
-      /* display: flex; */
-    }
-
-    &::placeholder {
-      background: white;
-      color: lightgray;
-    }
   }
 
   .login-button {
