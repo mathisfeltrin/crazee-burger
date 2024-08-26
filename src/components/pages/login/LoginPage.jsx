65fcc28797d8a4ddea68a536177758da852c1437
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2024-08-26 13:46:05 +0200

    ✨  F04: ORDER > Navbar > Add Logo

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -5,7 +5,7 @@ import Logo from "../../reusable-ui/Logo";
 export default function LoginPage() {
   return (
     <LoginPageStyled>
-      <Logo />
+      <Logo className={"logo-login-page"} />
       <LoginForm />
     </LoginPageStyled>
   );
@@ -31,5 +31,10 @@ const LoginPageStyled = styled.div`
     right: 0;
     bottom: 0;
     z-index: -1;
+  }
+
+  .logo-login-page {
+    transform: scale(2.5);
+    margin-bottom: 40px;
   }
 `;
