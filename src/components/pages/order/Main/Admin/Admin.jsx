120315449f79f9a4b0cf096ea1684a62c836e1b1
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2024-09-02 17:42:13 +0200

    💄  F07: ADMIN > Tab > Add click and active style

--- src/components/pages/order/Main/Admin/Admin.jsx
+++ src/components/pages/order/Main/Admin/Admin.jsx
@@ -1,13 +1,16 @@
+import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
-import { theme } from "../../../../../theme";
 import AdminPanel from "./AdminPanel";
 import AdminTabs from "./AdminTabs";
 
 export default function Admin() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
   return (
     <AdminStyled>
-      <AdminTabs />
-      <AdminPanel />
+      <AdminTabs isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+      {!isCollapsed && <AdminPanel />}
     </AdminStyled>
   );
 }
