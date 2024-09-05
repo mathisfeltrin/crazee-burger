70bc388eb04a308da50bfa62b14adfc0472e9e38
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2024-09-05 18:10:30 +0200

    ♻️  F07: ADMIN > Tabs > Add scalable implementation

--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -34,7 +34,7 @@ export default function AdminTabs() {
           label={tab.label}
           Icon={tab.Icon}
           onClick={() => selectTab(tab.index)}
-          className={tab.className}
+          className={currentTabSelected === tab.index ? "is-active" : ""}
         />
       ))}
     </AdminTabsStyled>
